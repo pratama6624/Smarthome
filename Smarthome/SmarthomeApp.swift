ce7e473ab23941edc20e00f48339a09a4a0588ea
pratama6624 <scyoung6624@gmail.com>
2025-09-10 07:03:31 +0700

    Initial Commit

---
+++ Smarthome/SmarthomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmarthomeApp.swift
+//  Smarthome
+//
+//  Created by Pratama One on 10/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SmarthomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
