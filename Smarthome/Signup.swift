b4b029a57f7a21cff9a2491913c2d904d641b742
pratama6624 <scyoung6624@gmail.com>
2025-09-13 10:24:09 +0700

    View
    - Add Content View
    
    - Add ForgotPassword View
    
    - Add Home View
    
    - Add Login View
    
    - Add OnBoarding view
    
    - Add SignUp View
    
    - Add Welcome View
    
    - Add Font
    
    - Modify info.plist

---
+++ Smarthome/Signup.swift
@@ -0,0 +1,18 @@
+//
+//  Signup.swift
+//  Smarthome
+//
+//  Created by Pratama One on 12/09/25.
+//
+
+import SwiftUI
+
+struct Signup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Signup()
+}
---
+++ Smarthome/Welcome.swift
@@ -0,0 +1,18 @@
+//
+//  Welcome.swift
+//  Smarthome
+//
+//  Created by Pratama One on 11/09/25.
+//
+
+import SwiftUI
+
+struct Welcome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Welcome()
+}
